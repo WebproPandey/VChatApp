45ff7f8a98615f73f4c9117c676c0f404ab69b6f
Amit Pandey <amitpande184@gmail.com>
2024-10-07 15:02:38 +0530

    create room to join user  and show real time chat and also and discoonet functionlty

--- App.js
+++ App.js
@@ -1,15 +1,49 @@
 const express =  require("express")
 let app =  express()
 let indexRouter =  require("./routes/index")
-
 const path =  require("path")
+const  http =  require("http")
+const  socketIo =  require("socket.io")
+const { log } = require("console")
+const  server =  http.createServer(app)
+const io =  socketIo(server)
+
+let waitingUser = []
+let rooms = {}
+io.on("connection" ,function (socket) {
+    socket.on("joinRoom" ,  function(){
+        if(waitingUser.length > 0 ){
+          let partner =  waitingUser.shift()
+          
+          
+          const roomname =  `${socket.id}-${partner.id}`          
+          socket.join(roomname);
+          partner.join(roomname);
+
+          io.to(roomname).emit("joined" , roomname)
+        }
+        else{
+            waitingUser.push(socket)
+            
+        }
+    })
+    socket.on("disconnect" , function(){
+      let index =   waitingUser.findIndex(waitingUser => waitingUser.id === socket.id)
+      waitingUser.splice(index , 1)
+    })
+    socket.on("message" , function(data){
+     socket.broadcast.to(data.room).emit("message" , data.message)  
+           
+    })
+    
+})
 
 
 app.set("view engine" ,"ejs")
 app.use(express.json())
-app.use(express.urlencoded({extends:true}))
+app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname ,"public")))
 
 
 app.use("/" , indexRouter)
-app.listen(3000)
+server.listen(3000)
